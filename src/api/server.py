ae757d718d1f0b45c70825599de1a6a1a7923223
danwalk <danielwalker87@hotmail.com>
2021-07-06 09:21:46 +0200

    second commit

---
+++ src/api/server.py
@@ -0,0 +1,66 @@
+"""
+
+Crea una API flask con un solo endpoint que muestre por pantalla el json 'googleplaystore.json'
+de la carpeta /data. En resumen, el endpoint tiene que leer el fichero 'googleplaystore.json' y retornarlo.
+
+Además, este fichero contiene otra función que está fuera de la funcionalidad de la API flask
+
+"""
+
+import os, sys
+import pandas as pd
+from flask import Flask
+import json
+
+dir = os.path.dirname
+sep = os.sep
+src_path = dir(dir(os.path.abspath(__file__)))
+sys.path.append(src_path)
+
+project_path = dir(dir(dir(os.path.abspath(__file__))))
+sys.path.append(project_path)
+
+app = Flask(__name__)
+
+""" 1: No es una función de flask"""
+def mi_funcion():
+    from utils.flask_functions import funcion_flask_1
+    
+    """
+    TODO - Esta función ha de llamar a la función 'funcion_flask_1' que está en /utils/flask_functions.py y
+    mostrar por pantalla lo que retorne esa función.
+    """
+    return funcion_flask_1()
+
+@app.route("/") 
+def home():
+    mensaje = " <h1> DWALK_ML_PROJECT <h3>"
+    return mensaje
+
+@app.route('/home', methods=['GET'])
+def googleplaystore():
+    playstore_json = project_path + sep + 'data' + sep + 'googleplaystore.json'
+    json_playstore_readed = pd.read_json(playstore_json)
+    return json_playstore_readed.to_json()
+
+def main():
+    # Get the settings fullpath
+    settings_file = project_path + sep + 'config' + sep + 'flask_settings.json'
+    # Load json from file 
+    with open(settings_file, "r") as json_file_readed:
+        json_readed = json.load(json_file_readed)
+
+    # Load variables from jsons
+    SERVER_RUNNING = json_readed["server_running"]
+
+    if SERVER_RUNNING:
+        DEBUG = json_readed["debug"]
+        HOST = json_readed["host"]
+        PORT_NUM = json_readed["port"]
+        app.run(debug=DEBUG, host=HOST, port=PORT_NUM)
+    else:
+        print("Server settings.json doesn't allow to start server. " + "Please, allow it to run it.")
+        
+
+if __name__ == "__main__":
+    main()
---
+++ src/dashboard/app.py
@@ -0,0 +1,73 @@
+import streamlit as st
+from PIL import Image
+import os, sys
+import pandas as pd
+import json
+import pymysql
+from sqlalchemy import create_engine
+
+
+# Haz que se pueda importar correctamente estas funciones que están en la carpeta utils/
+dir = os.path.dirname
+sep = os.sep
+src_path = dir(dir(os.path.abspath(__file__)))
+sys.path.append(src_path)
+
+from utils.stream_config import draw_map
+from utils.dataframes import load_csv
+
+project_path = dir(dir(dir(os.path.abspath(__file__))))
+sys.path.append(project_path)
+
+
+menu = st.sidebar.selectbox('Menu:',
+            options=["Home", "Team Stats", "Game Predictor", "API", "Australia Fire"])
+
+if menu == "Home":
+    # Pon el título del proyecto que está en el archivo "config.json" en /config
+
+    config_json_path = project_path + sep + 'config' + sep + 'config.json'
+
+    with open(config_json_path, "r") as config_json_readed:
+        json_config = json.load(config_json_readed)
+
+    st.title(json_config['Title'])
+    st.write(json_config['Description'])
+    st.write("Win, Lose or Draw, this is entertainment, with numbers and statistics!")
+    
+if menu == "Team Stats":
+
+    # Carga la imagen que está en data/img/happy.jpg
+    path_image_nba = project_path + sep + 'data' + sep + 'img' + sep + 'nba background.jpg'
+    image = Image.open(path_image_nba)
+    st.image (image,use_column_width=True)
+
+if menu == "Game Predictor":
+    # El archivo que está en data/ con nombre 'red_recarga_acceso_publico_2021.csv'
+    nbacsv = project_path + sep + 'data' + sep + 'nbaclean.csv'
+    df_nba = pd.read_csv(nbacsv)
+    st.dataframe(df_nba)
+
+if menu == "API":
+    # Accede al único endpoint de tu API flask y lo muestra por pantalla como tabla/dataframe
+    st.title('Play Store')
+    dataframe_playstore = pd.read_json('http://localhost:6060/playstore')
+    st.table(dataframe_playstore)
+    st.balloons()
+
+
+if menu == "Australia Fire":
+    """6"""
+    st.title("Fire Table")
+    
+
+    # 1. Conecta a la BBDD
+    # 2. Obtén, a partir de sentencias SQL (no pandas), la información de las tablas que empiezan por 'fire_archive*' (join)
+    # 3. Entrena tres modelos de ML diferentes siendo el target la columna 'fire_type'. Utiliza un pipeline que preprocese los datos con PCA. Usa Gridsearch.  
+    # 4. Añade una entrada en la tabla 'student_findings' por cada uno de los tres modelos. 'student_id' es EL-ID-DE-TU-GRUPO.
+    # 5. Obtén la información de la tabla 'fire_nrt_M6_96619' y utiliza el mejor modelo para predecir la columna target de esos datos. 
+    # 6. Usando SQL (no pandas) añade una columna nueva en la tabla 'fire_nrt_M6_96619' con el nombre 'fire_type_EL-ID-DE-TU-GRUPO'
+    # 7. Muestra por pantalla en Streamlit la tabla completa (X e y)
+    
+
+
